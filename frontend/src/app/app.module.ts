ad9ca4328808c7fcb7a2b2e053151c3d8cc94634
Ravinder Singh <46300631+devravindersingh@users.noreply.github.com>
2019-10-01 01:43:16 +0530

    frontend in progress
    designing is pending, controllers, and services will be done soon.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,27 @@ import { GuestModeComponent } from './guest-mode/guest-mode.component';
 import { StudentModeComponent } from './student-mode/student-mode.component';
 import { MentorModeComponent } from './mentor-mode/mentor-mode.component';
 import { FooterSectionComponent } from './footer-section/footer-section.component';
+import { StudentRegisterComponent } from './student-register/student-register.component';
+import { MentorRegisterComponent } from './mentor-register/mentor-register.component';
+import { LoginComponent } from './login/login.component';
+import { CoursesViewComponent } from './courses-view/courses-view.component';
+import { PaymentViewComponent } from './payment-view/payment-view.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+import { HomeViewComponent } from './home-view/home-view.component';
+
+const appRoutes:Routes = [
+  {path:'', component: HomeViewComponent},
+  {path:'guest', component: GuestModeComponent},
+  {path:'student', component: StudentModeComponent},
+  {path:'mentor', component: MentorModeComponent},
+  {path:'footer', component: FooterSectionComponent},
+  {path:'studentRegister', component: StudentRegisterComponent},
+  {path:'mentorRegister', component: MentorRegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'courses', component: CoursesViewComponent},
+  {path:'payment', component: PaymentViewComponent},
+  {path:'profile', component: ProfileViewComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,12 +36,19 @@ import { FooterSectionComponent } from './footer-section/footer-section.componen
     GuestModeComponent,
     StudentModeComponent,
     MentorModeComponent,
-    FooterSectionComponent
+    FooterSectionComponent,
+    StudentRegisterComponent,
+    MentorRegisterComponent,
+    LoginComponent,
+    CoursesViewComponent,
+    PaymentViewComponent,
+    ProfileViewComponent,
+    HomeViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
